3d52573e21a3b9b179d5ba5fd01428fa343acced
shaunwegs <96139139+shaunwegs@users.noreply.github.com>
2024-08-21 09:14:16 -0500

    Update style.js

--- 2024/focs/q/8_21/style.js
+++ 2024/focs/q/8_21/style.js
@@ -68,15 +68,7 @@ if (userLastName === null) {
 	localStorage.setItem('focs0_userLastName', userLastName);}
 
 var questions = [
-	
-				[
-		"What is the keyboard shortcut to take a screenshot on the mac? ",
-		"control + print screen + 3",
-		"shift + print screen",
-		"option + command + print screen",
-		"command + shift + 3",
-		"control + shift + 3",
-		"d", "d", "d" ],
+	[
 				[
 		"The acronym CTE is short for ",
 		"Career, Technology, & Entrepreneurship",
