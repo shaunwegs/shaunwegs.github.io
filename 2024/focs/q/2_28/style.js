a0e3070b853221642ae7687a08f2449df92b0794
S.Weg <96139139+shaunwegs@users.noreply.github.com>
2025-02-26 08:25:32 -0600

    Update style.js

--- 2024/focs/q/2_28/style.js
+++ 2024/focs/q/2_28/style.js
@@ -1080,10 +1080,11 @@ function renderResults(){
   $_('timeleft').innerHTML = '</h2>'; l1 = 25 - cor;
   test.innerHTML = "";
   test.innerHTML += "<h1>Results for " + userFirstName + "</h1>";
-    test.innerHTML += "<table id='render'><tr><td><p id='render1'></p><br></td><td><p id='render2'></p></td></tr></table>";
+    test.innerHTML += "<table id='render'><tr><td><p id='render1'></p><br></td><td><br><p id='render2'></p></td></tr></table>";
   render1.innerHTML += "<h1>Percentage Correct</h1><br>"
   render1.innerHTML += bigLet((cor/rscore*100).toFixed(0));
-  render2.innerHTML += "<br><br><br><h3>Attempt #"+vs +" for " + userFirstName +" "+ userLastName +".<br>Screenshot & upload your results!</h3><h1>Categories</h1>"
+  render1.innerHTML += "<br><br>"
+  render2.innerHTML += "<br><br><br><br><br><br><h3>Attempt #"+vs +" for " + userFirstName +" "+ userLastName +".<br>Screenshot & upload your results!</h3><h1>Categories</h1>"
   render2.innerHTML += "<br>Correct Answers:<br>"
   render2.innerHTML += "<span>" + showNum(corrs[0]) + showOf() + showNum(totas[0]) + "</span><span>" + totasNames[0] + "</span><br>";
   render2.innerHTML += "<span>" + showNum(corrs[1]) + showOf() + showNum(totas[1]) + "</span><span>" + totasNames[1] + "</span><br>";
