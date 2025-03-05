c10df188347786114bd346d0efff4804fe52ccf1
S.Weg <96139139+shaunwegs@users.noreply.github.com>
2025-03-05 16:39:10 -0600

    Update style.js

--- 2024/pit/q/3_3/style.js
+++ 2024/pit/q/3_3/style.js
@@ -951,7 +951,9 @@ function renderResults(){
   render1.innerHTML += "<br><h1>% Correct</h1><br>"
   render1.innerHTML += bigLet((cor/rscore*100).toFixed(0));
   render1.innerHTML += "<br><br>"
-  render2.innerHTML += "<br><h3>Attempt #"+vs +" for " + userFirstName +" "+ userLastName +"<br> for March Programming Quiz.<br>Screenshot & upload your results!</h3><br><br><h1>Categories</h1>"
+  render2.innerHTML += "<br><h3>Attempt #"+vs +" for " + userFirstName +" "+ userLastName +"<br> for March Programming Quiz."
+  render2.innerHTML += now();
+  render2.innerHTML += "<h3>Screenshot & upload your results!</h3><h1>Categories</h1>"
   render2.innerHTML += "<br>Correct Answers:<br>"
   render2.innerHTML += "<span>" + showNum(corrs[0]) + showOf() + showNum(totas[0]) + "</span><span>" + totasNames[0] + "</span><br>";
   render2.innerHTML += "<span>" + showNum(corrs[1]) + showOf() + showNum(totas[1]) + "</span><span>" + totasNames[1] + "</span><br>";
