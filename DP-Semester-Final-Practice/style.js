9b05596c6daa393579c8db319fe6872194278127
S.Weg <96139139+shaunwegs@users.noreply.github.com>
2025-12-08 13:47:21 -0600

    Update style.js

--- DP-Semester-Final-Practice/style.js
+++ DP-Semester-Final-Practice/style.js
@@ -1256,7 +1256,7 @@ function noMore(){
 
 function renderQuestion() {
   var test = $_("test");
-  var ques = 20;
+  var ques = 33;
   $_("test_status").innerHTML = "<h1>DP CS Practice Fall Final</h1><h2>- Student: "+userFirstName + " " + userLastName+"<br> - Attempt #"+window.vs +" - Q: "+(pos+1)+"/"+ques+"</h2>";
   if (rscore != 0) { $_("test_status").innerHTML += '<br>Currently: ~'+(cor/rscore*100).toFixed(0)+'% correct'; }
   var question = questions[posn][0];
